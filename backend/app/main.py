1a4a7edf2c3260ddd16209a86a230f753d9eae8b
Amilton Carvalho Jr <amilton@email.com>
2026-05-29 15:27:17 -0300

    "Update main.py and add index.html template"

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-from backend.app.database.database import engine, Base
+from backend.app.database.database import Base, SessionLocal, engine
 
 from backend.app.models.client import Client
 from backend.app.models.schedule import Schedule
@@ -9,6 +12,30 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "ColetaFlow API Online"}
+templates = Jinja2Templates(directory="backend/app/templates")
+
+app.mount(
+    "/static",
+    StaticFiles(directory="backend/app/static"),
+    name="static"
+)
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+
+    db = SessionLocal()
+
+    clientes = db.query(Client).all()
+    schedules = db.query(Schedule).all()
+
+    db.close()
+
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "clientes": clientes,
+            "schedules": schedules
+        }
+    )
